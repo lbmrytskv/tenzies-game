e9c083933ef84ee721576f625d5d1c8c302c443c
Liubomyr Yatskiv <liubomyryatskiv97@gmail.com>
2023-03-15 19:58:21 +0100

    changed an arrayto be an array of objects instead of array of numbers

--- src/App.js
+++ src/App.js
@@ -9,19 +9,21 @@ export default function App() {
     //created state to hold our array of numbers
 const [dice, setDice] = React.useState(allNewDice())
 
-//function that returns an array of 10 random numbers between 1 and 6
+//function that returns an array of objects
 function allNewDice() {
     const newDice = []
     //loop 10 times
     for (let i=0; i<10; i++) {
-        //create a random number from 1 to 6, ceil- floors the number to a bigger one (0.986=1)
-        newDice.push(Math.ceil(Math.random()*6))
-    }
+        newDice.push({
+            value: Math.ceil(Math.random()*6),
+            isHeld: false
+
+    })}
   return newDice
 }
 //passed prop to a die component through map method
 const diceElements = dice.map(function(die){
-    return <Die value={die}/>
+    return <Die value={die.value}/>
 })
 
 //
