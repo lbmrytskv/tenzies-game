b3a32297531f9c624405b979f28891876a44d152
Liubomyr Yatskiv <liubomyryatskiv97@gmail.com>
2023-03-15 18:39:21 +0100

    initialized state and properties

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
+
 import React from "react"
 import Die from "./Die"
+
+
+
 export default function App() {
+
+    //created state to hold our array of numbers
+const [dice, setDice] = React.useState(allNewDice())
+
+//function that returns an array of 10 random numbers between 1 and 6
+function allNewDice() {
+    const newDice = []
+    //loop 10 times
+    for (let i=0; i<10; i++) {
+        //create a random number from 1 to 6
+        newDice.push(Math.ceil(Math.random()*6))
+    }
+  return newDice
+}
+//passed prop to a die component through map method
+const diceElements = dice.map(function(die){
+    return <Die value={die}/>
+})
+
     return (
         <main>
-            Тискінис
             <div className="dice-container">
-            <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-
+            {diceElements}
             </div>
 
         </main>
